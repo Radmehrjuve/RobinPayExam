6ba6d353081b1a1aa1ede7c52d193ee94547dc3b
Radmehr <Radmehrvosooghi@gmail.com>
2024-05-05 02:00:44 +0330

    Add project files.

---
+++ Question01/Program.cs
@@ -0,0 +1,21 @@
+﻿int ArraySize = 0;
+int MinimumSize = 0;
+Console.Write("Input number of string to store in the Array: ");
+ArraySize = int.Parse(Console.ReadLine());
+string[] array = new string[ArraySize];
+Console.WriteLine($"Input {ArraySize} strings for the Array:");
+for(int i = 0; i < ArraySize; i++)
+{
+    Console.Write($"Element[{i}] : ");
+    array[i] = Console.ReadLine();
+}
+Console.Write("Input the minimum length of the item you want to find: ");
+MinimumSize = int.Parse(Console.ReadLine());
+Console.WriteLine($"The items of minimum {MinimumSize} characters are: ");
+foreach (string s in array)
+{
+    if(s.Length >= MinimumSize)
+        Console.WriteLine("Item: " + s);
+    
+}
+Console.ReadLine();
