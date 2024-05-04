46bbe559194133a6c1c0befb44f5988feccd804b
Radmehr <Radmehrvosooghi@gmail.com>
2024-05-05 02:20:32 +0330

    feat: Question 3
    Question 3 completed.

---
+++ Question3/Program.cs
@@ -0,0 +1,11 @@
+﻿//Question3
+//To use it, Set it as Startup Project in Solution Explorer
+string[] strings = new string[2];
+string Status = string.Empty;
+Console.WriteLine("Write 2 strings:");
+strings[0] = Console.ReadLine(); 
+strings[1] = Console.ReadLine();
+Console.WriteLine($"Orijinal Strings: {strings[0]}, {strings[1]}");
+bool result = strings[0].SequenceEqual(strings[1]);
+Console.WriteLine($"Check the same character pattern: {result}");
+    
